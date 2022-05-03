14b219c133087b3c078ab2081e8f93be24b07b26
Albin Dahlén <b21albda@student.his.se>
2022-05-03 13:07:17 +0200

    Implemented secondactivity

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.project;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -21,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         myPreferenceRef = getPreferences(MODE_PRIVATE);
         myPreferenceEditor = myPreferenceRef.edit();
+
+        Button btn = findViewById(R.id.button);
+
+        btn.setOnClickListener(View -> {
+            Intent intent = new Intent(this, SecondActivity.class);
+            startActivity(intent);
+        });
     }
 
 }
