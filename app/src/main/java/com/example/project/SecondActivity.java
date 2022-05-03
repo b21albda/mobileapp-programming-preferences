2cd64bdbf7d5b09713d57ad80040923d16341c53
Albin Dahlén <b21albda@student.his.se>
2022-05-03 13:11:20 +0200

    Stored input in preferences

--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -2,13 +2,32 @@ package com.example.project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private SharedPreferences myPreferenceRef;
+    private SharedPreferences.Editor myPreferenceEditor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        myPreferenceRef = getPreferences(MODE_PRIVATE);
+        myPreferenceEditor = myPreferenceRef.edit();
+
+        Button btn = findViewById(R.id.btn_save);
+
+        btn.setOnClickListener(View -> {
+            EditText input = findViewById(R.id.input);
+
+            myPreferenceEditor.putString("INPUT", input.getText().toString());
+            myPreferenceEditor.apply();
+            finish();
+        });
     }
 }
